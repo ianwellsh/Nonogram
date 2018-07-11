3fd7e7ab1bd0bb4cd14f04dfaf6bbd14d9485e71
Ian Wells <ianwellsh@hotmail.com>
2018-07-11 11:45:54 -0400

    commit
    ups

--- main.py
+++ main.py
@@ -10,28 +10,26 @@ def rules(nonogram,rowHints,columnHints,n,m):
     #pistas=Pistas.split()
     x=0
     y=0
-    
     for listP in rowHints:
-        
         #Si dentro de las pistas encuentra un 0, rellena toda la fila con 1
-        if(listP[x]==0):
-            
+        if(listP==0):
             for y in range(m):
                 nonogram[x,y]=1
-        if(listP[x]==m):
+        if(listP==m):
             for y in range(m):
                 nonogram[x,y]=2
 
         x=x+1
-    
+        
     for listP in columnHints:
-        if(listP[y]==0):
+        if(listP==0):
             for x in range(n):
                 nonogram[x,y]=1
-        if(listP[y]==n):
+        if(listP==n):
             for x in range(m):
                 nonogram[x,y]=2
         y=y+1
+    print(nonogram)
     return nonogram
 
 def printNonogram(nonogram):
@@ -66,7 +64,7 @@ for i in range(m):
     for element in hints.split():
         elementList.append(int(element)) #se transforman los elementos en numero
         columnHints.append(elementList)
-print(type(rowHints[0]))
-print(type(columnHints))
-#nonogram = rules(nonogram,rowHints,columnHints,n,m)
-printNonogram(nonogram)
+print(rowHints)
+print(columnHints)
+nonogram = rules(nonogram,rowHints,columnHints,n,m)
+#printNonogram(nonogram)
